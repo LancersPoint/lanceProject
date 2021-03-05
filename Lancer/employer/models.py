bb94b468cc164d9c53291f75e8e6715c9db34b1c
michaellyon <michaellyon@localhost.localdomain>
2021-03-05 12:49:20 +0100

    Please help me look at the models

--- Lancer/employer/models.py
+++ Lancer/employer/models.py
@@ -19,17 +19,14 @@ class Posts(models.Model):
     published = PublishedManager() # to easilyy retrieve published posts
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='blog_posts')
+    employer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='jobs_posted')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='draft')
-    bids = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                  related_name='bidders',
-                                  blank=True)
 
     #Todo: add a conical url. Absolute url to see details about a post
     class Meta:
@@ -37,3 +34,21 @@ class Posts(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Bidders(models.Model):
+    post = models.ForeignKey(
+        Posts, on_delete=models.CASCADE, related_name='comments')
+    message = models.TextField()
+    from_who = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                        related_name='sender',
+                                        on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Message by {self.from_who} on {self.post}'
--- Lancer/freelancer/models.py
+++ Lancer/freelancer/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.conf import settings
+from employer.models import Posts
 
 class Gig(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             related_name='gigs_bidded',
+                             related_name='bidders',
                              on_delete=models.CASCADE)
+    bid = models.ManyToManyField(Posts, blank=True, related_name="bids")
     
     
